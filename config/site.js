332f435d3797b4968279490fc211b3a85438a0cb
Tommy Saputra <contact@tommysaputra.com>
2023-07-12 10:51:44 +1000

    updated gitignore, added more packages like three.js, r3f, drei, and etc. added config folder

---
+++ config/site.js
@@ -0,0 +1,7 @@
+export const siteConfig = {
+    name: "The Kwan Sista",
+    description:
+        "Freshly made with love",
+    url: process.env.NEXT_PUBLIC_APP_URL,
+    ogImage: "https://thekwansista.com/photo.jpg",
+}
